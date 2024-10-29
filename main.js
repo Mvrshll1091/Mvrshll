9f51b3ffd7bc800bb371182410a055d61522556b
Mvrshll <cammarshxll@gmail.com>
2024-10-29 15:26:43 -0400

    Mvrshll 1.1
    Latest iteration of my website files.

---
+++ main.js
@@ -0,0 +1,46 @@
+    function Section1_visble() {
+        Section1.style.display = "inline-block";
+        Section2.style.display = "none";
+        Section3.style.display = "none";
+        Section4.style.display = "none";
+        Section5.style.display = "none";
+    }
+    
+    function Section2_visble() {
+        Section1.style.display = "none";
+        Section2.style.display = "inline-block";
+        Section3.style.display = "none";
+        Section4.style.display = "none";
+        Section5.style.display = "none";
+    }
+    
+    function Section3_visble() {
+        Section1.style.display = "none";
+        Section2.style.display = "none";
+        Section3.style.display = "inline-block";
+        Section4.style.display = "none";
+        Section5.style.display = "none";
+    }
+
+    function Section4_visble() {
+        Section1.style.display = "none";
+        Section2.style.display = "none";
+        Section3.style.display = "none";
+        Section4.style.display = "inline-block";
+        Section5.style.display = "none";
+    }
+
+    function Section5_visble() {
+        Section1.style.display = "none";
+        Section2.style.display = "none";
+        Section3.style.display = "none";
+        Section4.style.display = "none";
+        Section5.style.display = "inline-block";
+    }
+    
+    var Section1 = document.getElementById("Section1");
+    var Section2 = document.getElementById("Section2");
+    var Section3 = document.getElementById("Section3");
+    var Section4 = document.getElementById("Section4");
+    var Section5 = document.getElementById("Section5");
+    
